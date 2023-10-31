116088ece8137bd450290f782be5dd078b1379b6
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-10-31 17:55:42 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+'use strict'; // treat all JS Code as newer version of JS
+
+// alert('3 + 3')  -- we are using nodeJS not browser that's why it shows error
+// code readability must be high
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
