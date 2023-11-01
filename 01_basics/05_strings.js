f9f18a393c992477ca3585573a3e5c6ec1f5b69a
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-11-01 18:17:24 +0000

    05_strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,3 +11,17 @@ console.log(newGameName);
 
 const anotherGameName = gameName.slice(-8,4)
 console.log(anotherGameName)
+
+const newStringOne = '      Amit        '
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = 'https://amit.com/amit%20gupta'
+console.log(url);
+console.log(url.replace('%20','-'));
+console.log(url.includes('amit'));
+
+const newStringTwo = 'Amit-hc-kr'
+console.log(newStringTwo.split('-'));
+
+
