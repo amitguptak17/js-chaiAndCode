0723188b7c25438efe5dda3d18bdbc53b5902aef
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-11-01 11:19:14 +0000

    04_comaparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+// avoid these type of comparison, try to write clean code
