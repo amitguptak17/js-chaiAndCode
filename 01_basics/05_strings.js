b990be049d0249df69ae8ba734d26320154527d5
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-11-01 13:13:41 +0000

    05string - 01

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const gameName = new String('Amit-nv');
+const myName = 'honey'
+console.log(gameName);
+
+//console.log(gameName.charAt(2))
+//console.log('bharat'.charAt(2))
+//console.log(gameName.indexOf('u'))
+
+let newGameName = gameName.substring(0,4)
+console.log(newGameName);
+
+const anotherGameName = gameName.slice(-8,4)
+console.log(anotherGameName)
