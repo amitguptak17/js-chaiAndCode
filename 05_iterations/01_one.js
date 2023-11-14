30069c0cf546d96c0ee040d8c2f9a04165c5e523
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-11-14 11:29:37 +0000

    01_one.js-iterations

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,16 @@
+// // for
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+for (let i = 0; i<=10; i++){
+    console.log(`------Outer Loop: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner Loop: ${j} `);cl
+        console.log(`${i} * ${j} = ${i*j}`);
+        
+    }
+}
+
