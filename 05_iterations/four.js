59dfce3ee7892edcd65cad66ff8d508f65c32692
amitguptak17 <86546667+amitguptak17@users.noreply.github.com>
2023-11-16 09:05:31 +0000

    for-of-loop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,5 +5,33 @@ const myNoCode = {
 }
 
 for (const key in myNoCode) {
-   console.log(myNoCode[key])
+   //console.log(myNoCode[key])
 }
+const myObject = {
+    javascript: 'js',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by Apple'
+}
+
+for (const key in myObject) {
+    //console.log(`Key for ${key} is ${myObject[key]}`);
+}
+
+const IndPlayers = ['Dhoni', 'Virat', 'Rohit', 'Shami', 'Bumrah']
+
+
+for (const key in IndPlayers) {
+   //console.log(IndPlayers[key]);
+        
+    }
+
+    const map = new Map()
+    map.set('IN', "India")
+    map.set('USA', "United States of America")
+    map.set('Fr', "France")
+    map.set('IN', "India")
+
+    for (const key in map) {
+        console.log(key);
+    }
